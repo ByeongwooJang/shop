9c43214d590a07185f2031218a24fc2d2e42717a
byeongwoojang <a01066278824@gmail.com>
2022-02-23 22:09:54 +0900

    thymeleaf 학습중

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,34 +1,122 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest // 통합 테스트를 위한 어노테이션
-@TestPropertySource(locations="classpath:application-test.properties") // 테스트 프로퍼티 세팅
+@TestPropertySource(locations = "classpath:application-test.properties") // 테스트 프로퍼티 세팅
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
+    @PersistenceContext
+    EntityManager em;
+
+    public void createItemList2() {
+        for (int i = 1; i <= 5; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품+i");
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+        for (int i = 6; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품+i");
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+
+
+    @Test
+    @DisplayName("상품 Querydsl 조회 테스트 2")
+    public void queryDslTest2() {
+        this.createItemList2();
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem item = QItem.item;
+
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        //booleanBuilder.and(item.price.gt(price));
+
+        if (StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+        //pageble 객체를 생성. 첫 번쨰 인자는 조회할 페이지 번호, 두번쨰 인자는 한 페이지당 조회할 데이터의 개수
+        Pageable pageable = PageRequest.of(2, 2);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + itemPagingResult.getTotalElements());
+
+        for (Item i : itemPagingResult.getContent()) {
+            System.out.println(i.toString());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemTest();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "Test Item Deatil" + "%"))
+                .orderBy(qItem.price.desc());
+
+        //fetch를 이용해서 쿼리 결과를 리스트로 반환.
+        List<Item> itemList = query.fetch();
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+
+    }
+
     //아래 두 어노테이션은 쌍으로 쓰는게 좋다. Test대상 지정, 테스트명을 의미
     @Test
     @DisplayName("상품 저장 테스트")
-    public void createItemTest(){
-        for(int i = 0; i < 10; i ++) {
+    public void createItemTest() {
+        for (int i = 0; i < 10; i++) {
             Item item = new Item();
-            item.setItemNm("Test Item"+i);
-            item.setPrice(10000+i);
-            item.setItemDetail("Test Item Detail"+i);
+            item.setItemNm("Test Item" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("Test Item Detail" + i);
             item.setItemSellStatus(ItemSellStatus.SELL);
             item.setStockNumber(100);
             item.setRegTime(LocalDateTime.now());
@@ -40,50 +128,52 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품명 조회 테스트")
-    public void findByItemNmTest(){
+    public void findByItemNmTest() {
         this.createItemTest();
         List<Item> itemList = itemRepository.findByItemNm("Test Item1");
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
 
     @Test
     @DisplayName("상품명, 상품상세설명 or 테스트")
-    public void findByItemNmOrItemDetailTest(){
+    public void findByItemNmOrItemDetailTest() {
         this.createItemTest();
         List<Item> itemList = itemRepository.findByItemNmOrItemDetail("Test Item2", "Test Item Detail7");
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
 
     @Test
     @DisplayName("가격 LessThan 테스트")
-    public void findByPriceLessThanTest(){
+    public void findByPriceLessThanTest() {
         this.createItemTest();
         List<Item> itemList = itemRepository.findByPriceLessThan(10005);
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
+
     @Test
     @DisplayName("가격 LessThan 내림차순 테스트")
-    public void findByPriceLessThanOrderByPriceDescTest(){
+    public void findByPriceLessThanOrderByPriceDescTest() {
         this.createItemTest();
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
 
     @Test
     @DisplayName("Query를 이용한 상품 조회 테스트")
-    public void findByItemDetailTest(){
+    public void findByItemDetailTest() {
         this.createItemTest();
         List<Item> itemList = itemRepository.findByItemDetail("Test Item Detail");
-        for(Item item : itemList){
+        for (Item item : itemList) {
             System.out.println(item.toString());
         }
     }
+
 }
